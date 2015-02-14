bb1373bd7243230ec5dee3cb4e92d81dd66ba587
Enrique <huanchaco33@hotmail.com>
2015-02-14 23:04:27 +0100

    add bundle AdpBackendBundle et AdpFrontendBundle

---
+++ src/Adp/BackendBundle/AdpBackendBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Adp\BackendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdpBackendBundle extends Bundle
+{
+}
---
+++ src/Adp/FrontendBundle/AdpFrontendBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Adp\FrontendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AdpFrontendBundle extends Bundle
+{
+}
